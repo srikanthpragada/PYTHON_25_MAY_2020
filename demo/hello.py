3d71ddf472e5a7e47572c518e0d2f2837fa2f3d7
srikanthpragada <srikanthpragada@gmail.com>
2020-05-28 11:00:24 +0530

    27-MAY

---
+++ demo/hello.py
@@ -0,0 +1,5 @@
+# Program to say hello
+
+name = input("Enter your name :")
+print("Hello", name)
+
---
+++ demo/leap_year.py
@@ -0,0 +1,8 @@
+# Program to check whether given year is leap year
+
+year = int(input("Enter year :"))
+if year % 4 == 0:
+    print("Leap Year")
+else:
+    print("Not a leap year")
+
