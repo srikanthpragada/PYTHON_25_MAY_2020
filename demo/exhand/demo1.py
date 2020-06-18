77a1db6caf63707588333296d0a9bed275c2833f
srikanthpragada <srikanthpragada@gmail.com>
2020-06-18 19:57:04 +0530

    18-JUNE

--- demo/exhand/demo1.py
+++ demo/exhand/demo1.py
@@ -1,3 +1,13 @@
 l = [1, 2, 3]
 
-l.add(4)
+try:
+    l.add(4)
+    print("Done")
+except ValueError:
+    print("Value Error")
+except Exception as ex:
+    print("Error :", ex)
+else:
+    print("Success")
+
+print("Over")
