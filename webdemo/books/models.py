2bed5c2f0fd0c82ba6ff574c75a6a3c4239ba7ff
srikanthpragada <srikanthpragada@gmail.com>
2020-07-03 19:57:09 +0530

    03-JULY

---
+++ webdemo/books/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=30, unique=True, null=False)
+    author = models.CharField(max_length=20, null=False)
+    price = models.IntegerField(null=False)
+
+    def __str__(self):
+        return f"{self.id} - {self.title} - {self.author} - {self.price}"
